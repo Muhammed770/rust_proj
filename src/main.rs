b972c945614f5dc604445dbd2040e8d359d183e3
muhammed770 <mail.muhammed2002@gmail.com>
2024-06-10 07:29:33 +0530

    second

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
 fn main() {
     let greetings = String::from("hello world");
-    for i in 0..greetings.len()+10 {
-        let char1 = greetings.chars().nth(i);
-        match char1 {
-            Some(c) => print!("{} ", c),
-            None => {
-                println!();
-                println!("no char found at {} :", i);
-                break;
-            }
-        }
-    }
+    let first_word = get_first_word(&greetings);
+    // for i in 0..greetings.len()+10 {
+    //     let char1 = greetings.chars().nth(i);
+    //     match char1 {
+    //         Some(c) => print!("{} ", c),
+    //         None => {
+    //             println!();
+    //             println!("no char found at {} :", i);
+    //             break;
+    //         }
+    //     }
+    // }
     println!();
-    println!("{}", greetings);
+    println!("{}",greetings);
+    println!("the first word is :{}", first_word);
+}
+
+fn get_first_word(sentence:&str) -> String {
+    let mut ans = String::new();
+    for c in sentence.chars() {
+        if c.is_whitespace() {
+            break;
+        }
+        ans.push(c)
+    }
+    return ans;
 }
